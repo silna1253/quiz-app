529fb725dd2bd125eb9b2a76b6869528a80dd8a7
silna1253 <silnavincent20@gmail.com>
2025-04-15 20:00:32 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,48 @@
+export const data = [
+    {
+        question: "A queue follows which order?",
+        option1: "LIFO",
+        option2: "FILO",
+        option3: "FIFO",
+        option4: "None",
+        ans: 3,
+    },
+    {
+        question: "What data structure is used in a recursive function call?",
+        option1: "Queue",
+        option2: "Stack",
+        option3: "Linkedlist",
+        option4: "Array",
+        ans: 2,
+    },
+    {
+        question: "Which sorting algorithm is best for nearly sorted arrays?",
+        option1: "Bubble Sort",
+        option2: "Quick Sort",
+        option3: "Insertion Sort",
+        option4: "Merge Sort",
+        ans: 3,
+    },
+   
+    {
+        question: "Which type of memory is used for BIOS?",
+        option1: "EEPROM", 
+        option2:"RAM",
+        option3: "DRAM",
+        option4:"Cache",
+        ans: 1,
+    },
+   
+  
+    {
+        question:"Which traversal method is used for printing expression trees?",
+        option1:"Levelorder",
+        option2:"Preorder",
+        option3:"Postorder",
+        option4:"Inorder",
+        ans: 4,
+    },
+    
+ 
+
+]
